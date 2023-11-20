76ae65577acd2b9d62630d102345803343544696
qyao4 <138896505+qyao4@users.noreply.github.com>
2023-11-20 15:14:00 -0600

    5.4 SEO
    Add .htaccess file and check view.php

--- public/view.php
+++ public/view.php
@@ -6,6 +6,8 @@
     Description: view and comment vehicle details(non-administrative)
 
     ****************/
+    $baseURL = "http://" . $_SERVER['HTTP_HOST'] . rtrim(dirname($_SERVER['PHP_SELF']), '/\\') . "/";
+
     $need_authenticated = false; //non-administrative
     
     if (session_status() == PHP_SESSION_NONE)
@@ -21,6 +23,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <base href="<?php echo $baseURL; ?>">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
